fb3457b21859010c15c38d0ac317d919123a448e
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-03-07 17:50:08 -0500

    Add tests for course and dish models.

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "courses exist" do
+    assert Course.new
+  end
+
+  test "course has many dishes" do
+    assert Course.dishes
+  end
+  
 end
--- test/models/dish_test.rb
+++ test/models/dish_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
+
 class DishTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "dishes exist" do
+    assert Dish.new
+  end
+
+  test "dishes belong to a course" do
+    ramen = Dish.new(name: "Tonkotsu", description: "yummy")
+    entree = Course.new(name: "entree")
+    entree << ramen
+    assert_equal 1, dishes(:one).course_id
+    assert_equal 1, ramen.course_id
+  end
+
 end
