c92452dacbe3f92b4ad1f12cc8b3f8c9f05e468f
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-03-08 16:09:34 -0500

    Fixed it all yo. Run that shit.

---
+++ app/models/references.rb
@@ -0,0 +1,9 @@
+for vies/_form.html.erb
+
+  <!-- <div class="field">
+    <% if @dish.persisted? %>
+      <%= f.label :course_id %><br>
+      <%= f.collection_select :course_id, Course.all, :id, :name %>
+    <% else %>
+      <%= f.hidden_field :course_id, Course.all, :id, :name %>
+  </div> -->
